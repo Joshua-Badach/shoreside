68169084aa12f8381c4d0c97d39708334a8d22e7
jbadach02 <joshuabadach@gmail.com>
2022-05-20 16:11:39 -0600

    fixed a typo... must be friday

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ get_header()
     <div class="row">
         <div class="offset-lg-7 col-lg-5">
             <h1 class="errorHeading">404</h1>
-            <p>Lost at sea? Let's get you back on board!</p>Some functions changes
+            <p>Lost at sea? Let's get you back on board!</p>
         </div>
     </div>
 </div>
