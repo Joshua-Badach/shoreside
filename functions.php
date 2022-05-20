96fb5fd21a56f07d3b91548e29abafc864d557fb
jbadach02 <joshuabadach@gmail.com>
2022-05-20 13:57:14 -0600

    Cleaning up functions, added logo functionality

--- functions.php
+++ functions.php
@@ -2,38 +2,49 @@
 
 if ( ! function_exists( 'rpsShoreside_setup') ):
 
-function rpsShoreside_setup(){
-    load_theme_textdomain( 'rpsShoreside', get_template_directory() . '/languages');
+    function rpsShoreside_setup(){
+        load_theme_textdomain( 'rpsShoreside', get_template_directory() . '/languages');
 
-    register_nav_menus( array(
-        'primary'   => __( 'Primary Menu', 'rpsShoreside' ),
-        'secondary' => __( 'Secondary Menu', 'rpsShoreside')
+        register_nav_menus( array(
+            'primary'   => __( 'Primary Menu', 'rpsShoreside' ),
+            'secondary' => __( 'Secondary Menu', 'rpsShoreside')
 
-    ) );
-    add_theme_support( 'title-tag' );
-    add_theme_support(
-        'html5',
-        array(
-            'search-form',
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption',
-            'script',
-            'style',
-            'navigation-widgets',
-        )
-    );
-    add_theme_support( 'editor-styles' );
+        ) );
+//    add_theme_support( 'title-tag' );
+//    add_theme_support(
+//        'html5',
+//        array(
+//            'search-form',
+//            'comment-form',
+//            'comment-list',
+//            'gallery',
+//            'caption',
+//            'script',
+//            'style',
+//            'navigation-widgets',
+//        )
+//    );
+//    add_theme_support( 'editor-styles' );
+    function shoreside_logo_setup() {
+        $defaults = array(
+            'height'                  =>400,
+            'width'                   =>300,
+            'flex-height'             =>true,
+            'flex-width'              =>true,
+            'header-text'             =>array( 'site-title', 'site-description' ),
+           'unlink-homepage-logo'    =>true, );
+        add_theme_support('custom-logo', $defaults);
+    }
+    add_action( 'after_setup_theme', 'shoreside_logo_setup' );
 
-    //    Enqueue bundles - do not touch!
+
     function add_theme_scripts(){
         wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/dist/main.bundle.js', false );
         wp_enqueue_style( 'bundle', get_template_directory_uri() . '/assets/dist/bundle.css', false );
     }
-
-    //    Run Functions
     add_action( 'wp_enqueue_scripts', 'add_theme_scripts');
+
 }
 endif;
+
 add_action( 'after_setup_theme', 'rpsShoreside_setup');
