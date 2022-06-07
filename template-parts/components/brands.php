b7109dcb43f1e89d1a87f9a9b1dab6deb33b9cec
jbadach02 <joshuabadach@gmail.com>
2022-06-07 09:10:04 -0600

    continuing work on the brands component

--- template-parts/components/brands.php
+++ template-parts/components/brands.php
@@ -1,7 +1,9 @@
 <div class="col-sm-3">
     <a href="#">
         <div class="brands">
-            <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/avalon-logo-gray.png' ?>" alt="Avalon pontoons">
+            <div class="brandImage">
+                <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/avalon-logo-gray.png' ?>" alt="Avalon pontoons">
+            </div>
             <h5>Avalon</h5>
             <p>The Avalon brand is known for innovative, exciting, and luxurious pontoons. Avalon is a vertically integrated builder, meaning they make almost everything in-house from the pontoons, to framing, to furniture.</p>
         </div>
@@ -10,7 +12,9 @@
 <div class="col-sm-3">
     <a href="#">
         <div class="brands">
-            <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Mirrocraft-Logo.png' ?>" alt="Mirrocraft fishing boats">
+            <div class="brandImage">
+                <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Mirrocraft-Logo.png' ?>" alt="Mirrocraft fishing boats">
+            </div>
             <h5>Mirrocraft</h5>
             <p>The MirroCraft brand has become known for boat innovation, safety, and quality. We are proud to carry the full lineup of MirroCraft aluminum fishing boats and believe that the quality and price is unmatched by competitors. These boats are designed with performance and style in mind, so are perfect for both families new to boat ownership and experienced fishermen.</p>
         </div>
@@ -19,7 +23,9 @@
 <div class="col-sm-3">
     <a href="#">
         <div class="brands">
-            <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Mercury_Lockup.png' ?>" alt="Mercury outboard motors">
+            <div class="brandImage">
+                <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Mercury_Lockup.png' ?>" alt="Mercury outboard motors">
+            </div>
             <h5>Mercury</h5>
             <p>Mercury Marine is a leader in the industry and a staple among boaters and boat manufacturers. We are proud to offer the full lineup of Mercury outboards and kickers. Purchase with confidence knowing all new motors are backed by a Mercury 3 year Limited Warranty.</p>
         </div>
@@ -28,7 +34,9 @@
 <div class="col-sm-3">
     <a href="#">
         <div class="brands">
-            <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Shorestation.png' ?>" alt="Shorestation docking">
+            <div class="brandImage">
+                <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Shorestation.png' ?>" alt="Shorestation docking">
+            </div>
             <h5>Shorestation</h5>
             <p>ShoreStation is known for producing high quality and dependable waterfront equipment. Our boat lift systems and docks are made to last and backed by one of the best warranties in the business.</p>
         </div>
