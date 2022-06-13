ea9381eb0f4d01034fa7063ac911bae6592fe866
jbadach02 <joshuabadach@gmail.com>
2022-06-13 16:06:09 -0600

    screwing with the loop to return category specific posts

--- template-parts/components/carousel.php
+++ template-parts/components/carousel.php
@@ -4,7 +4,17 @@
     $home_query = new WP_Query( $args );
 
     if ($pageID == 0){
-        echo ('<h1> It Worked</h1>');
+        if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post();
+                if ( in_category( '2' )):
+                    echo ('<div> category worked </div>');
+                else:
+                    the_title();
+                endif;
+//                echo ('<h1> It Worked</h1>' . $pageID);
+            } // end while
+        } // end if
     }
     elseif ( $pageID == 1){
         echo ('<h1> This is page</h1>'. $pageID);
