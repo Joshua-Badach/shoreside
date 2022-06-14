776bea74fe77b29b46bc6ed843c449a3b012928f
jbadach02 <joshuabadach@gmail.com>
2022-06-14 09:00:52 -0600

    got the component pulling from the database and checking page id, just need to return filtered content by category now

--- template-parts/components/carousel.php
+++ template-parts/components/carousel.php
@@ -1,27 +1,29 @@
-<?php
-if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post();
-        ?>
+
 <div class="carousel">
-    <?php
+    <?php if ( have_posts() ) { while ( have_posts() ) { the_post();
+
     $pageID = get_queried_object_id();
     $home_query = new WP_Query( $args );
 
-    if ( $pageID == 0 ) :
-        echo ('category '. $pageID);
-    elseif ( $pageID == 1):
-        echo ('not the category you\'re looking for');
+    if ( $pageID == 1 ) :
+    ?><div class="tester"><?php
+
+        the_title();
+        the_content();
+        the_post_thumbnail();
+
+        ?></div><?php
+    else:
+//        change to elsif later
+        echo ('<div>not the category you\'re looking for</div>');
     endif;
 
-    ?>
-<!--    <img src="http://placekitten.com/1920/1080" alt="placeholder">-->
-<!--    <img src="http://placekitten.com/1920/1081" alt="placeholder">-->
-<!--    <img src="http://placekitten.com/1920/1082" alt="placeholder">-->
-<!--    <img src="http://placekitten.com/1920/1083" alt="placeholder">-->
-<!--    <img src="http://placekitten.com/1920/1084" alt="placeholder">-->
-
-</div>
-<?php } // end while
+//    <img src="http://placekitten.com/1920/1080" alt="placeholder">
+//    <img src="http://placekitten.com/1920/1081" alt="placeholder">
+//    <img src="http://placekitten.com/1920/1082" alt="placeholder">
+//    <img src="http://placekitten.com/1920/1083" alt="placeholder">
+//    <img src="http://placekitten.com/1920/1084" alt="placeholder">
+} // end while
     } // end if
     ?>
+</div>
