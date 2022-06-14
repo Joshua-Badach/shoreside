2c871eb323d2c5435129147852e9f894bed10b0c
jbadach02 <joshuabadach@gmail.com>
2022-06-14 15:56:21 -0600

    not pretty but working, work to modularize code later... I'm tired of looking at this

--- template-parts/components/carousel.php
+++ template-parts/components/carousel.php
@@ -2,19 +2,13 @@
     <?php
     $pageID = get_queried_object_id();
 
-//    $args = array(
-//        'post_type' => 'post',
-//        'category_name' => 'home-slider',
-//        'posts_per_page' => 5,
-//    );
-//    $homeQuery = new WP_Query( $args );
-
+    if ($pageID == 0):
     $query = new WP_Query(array( 'category_name' => 'home-slider'));
 
-    while ($query->have_posts() && $pageID == 0){
+    while ($query->have_posts()){
         $query->the_post();
 
-            ?><div class="col-sm-4 carouselItem"><?php
+            ?><div class="col-sm-4"><?php
                 the_title();
                 the_content();
                 the_post_thumbnail('full');
@@ -23,40 +17,54 @@
     }
 
     wp_reset_postdata();
-//
-//    $args2 = array(
-//        'post_type' => 'post',
-//        'category_name' => 'part-slider',
-//        'posts_per_page' => 5,
-//    );
-//    $partsQuery = new WP_Query( $args2 );
-//    while ($partQuery->have_posts() && $pageID == $args->post_id ){
-//    $partQuery->the_post();
-//
-//            ?><!--<div class="col-sm-4">--><?php
-//                the_title();
-//                the_content();
-//                the_post_thumbnail('full');
-//
-//            ?><!--</div>--><?php
-//    }
-//
-//    wp_reset_postdata();
-//    $the_post_id = get_the_ID();
-//    $article_terms = wp_get_post_terms( $the_post_id, ['category', 'post_tag']);
-//    print_r( $article_terms);
 
-//        if ( $pageID == 0 ) :
-//            if ( $homeQuery->have_posts() ) { while ( $homeQuery->have_posts() ) { $homeQuery->the_post();
-//
-//        ?><!--<div class="col-sm-4">--><?php
-//            the_title();
-//            the_content();
-//            the_post_thumbnail('full');
-//
-//            ?><!--</div>--><?php
-//                } // end while
-//            } // end if
-//        endif;
-//    ?>
+    elseif ($pageID == 43):
+    $query = new WP_Query(array( 'category_name' => 'showroom-slider'));
+
+    while ($query->have_posts()){
+    $query->the_post();
+
+        ?><div class="col-sm-4"><?php
+            the_title();
+            the_content();
+            the_post_thumbnail('full');
+
+        ?></div><?php
+    }
+
+    wp_reset_postdata();
+
+    elseif ($pageID == 66):
+        $query = new WP_Query(array( 'category_name' => 'parts-slider'));
+
+        while ($query->have_posts()){
+            $query->the_post();
+
+            ?><div class="col-sm-4"><?php
+            the_title();
+            the_content();
+            the_post_thumbnail('full');
+
+            ?></div><?php
+        }
+
+        wp_reset_postdata();
+
+        elseif ($pageID == 68):
+        $query = new WP_Query(array( 'category_name' => 'service-slider'));
+
+        while ($query->have_posts()){
+            $query->the_post();
+
+            ?><div class="col-sm-4"><?php
+            the_title();
+            the_content();
+            the_post_thumbnail('full');
+
+            ?></div><?php
+        }
+
+        wp_reset_postdata();
+    endif;
+   ?>
 </div>
