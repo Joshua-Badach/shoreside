b7dbf151f35c720cd4ca8e16f4859135ec28392a
jbadach02 <joshuabadach@gmail.com>
2022-06-14 08:45:09 -0600

    working on carousel logic and wordpress loop

--- template-parts/components/carousel.php
+++ template-parts/components/carousel.php
@@ -1,23 +1,27 @@
+<?php
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+        ?>
 <div class="carousel">
     <?php
     $pageID = get_queried_object_id();
     $home_query = new WP_Query( $args );
 
-    if ($pageID == 0){
-        if ( have_posts() ) {
-            while ( have_posts() ) {
-                the_post();
-                if ( in_category( '2' )):
-                    echo ('<div> category worked </div>');
-                else:
-                    the_title();
-                endif;
-//                echo ('<h1> It Worked</h1>' . $pageID);
-            } // end while
-        } // end if
-    }
-    elseif ( $pageID == 1){
-        echo ('<h1> This is page</h1>'. $pageID);
-    }
+    if ( $pageID == 0 ) :
+        echo ('category '. $pageID);
+    elseif ( $pageID == 1):
+        echo ('not the category you\'re looking for');
+    endif;
+
     ?>
+<!--    <img src="http://placekitten.com/1920/1080" alt="placeholder">-->
+<!--    <img src="http://placekitten.com/1920/1081" alt="placeholder">-->
+<!--    <img src="http://placekitten.com/1920/1082" alt="placeholder">-->
+<!--    <img src="http://placekitten.com/1920/1083" alt="placeholder">-->
+<!--    <img src="http://placekitten.com/1920/1084" alt="placeholder">-->
+
 </div>
+<?php } // end while
+    } // end if
+    ?>
