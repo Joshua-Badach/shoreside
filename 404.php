4097afa1ec4e451a6e5101b59494c48727a685eb
jbadach02 <joshuabadach@gmail.com>
2022-06-16 16:02:00 -0600

    quick and dirty fix for the 404, will address it later

--- 404.php
+++ 404.php
@@ -2,14 +2,15 @@
 get_header()
 ?>
 <div class="errorPage">
-<div class="container">
-    <div class="row">
-        <div class="offset-lg-7 col-lg-5">
-            <h1 class="errorHeading">404</h1>
-            <p>Lost at sea? Let's <a href="<?php echo site_url() ?>">get you back on board</a>!</p>
+    <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/404background.jpg' ?>" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="offset-lg-7 col-lg-5">
+                <h1 class="errorHeading">404</h1>
+                <p>Lost at sea? Let's <a href="<?php echo site_url() ?>">get you back on board</a>!</p>
+            </div>
         </div>
     </div>
-</div>
 </div>
 
 <?php
