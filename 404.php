5e947d0c0d202259f42b9ed401cb61cf1644a74e
jbadach02 <joshuabadach@gmail.com>
2022-06-17 07:52:44 -0600

    screwing with the 404 styling a bit when I got home last night

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@ get_header()
         <div class="row">
             <div class="offset-lg-7 col-lg-5">
                 <h1 class="errorHeading">404</h1>
-                <p>Lost at sea? Let's <a href="<?php echo site_url() ?>">get you back on board</a>!</p>
+                <p>Lost at sea? <a href="<?php echo site_url() ?>">Let's get you back on board</a>!</p>
             </div>
         </div>
     </div>
