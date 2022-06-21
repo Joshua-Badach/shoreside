9132b1cbb40d1984c823c1f349ab95c0e24d0670
jbadach02 <joshuabadach@gmail.com>
2022-06-21 10:17:00 -0600

    got content from wordpress, category name, description, id, and shildren id

--- template-parts/components/productGallery.php
+++ template-parts/components/productGallery.php
@@ -1,5 +1,15 @@
 <!--http://placekitten.com/200/200-->
-<?php $category = get_category_by_slug('showroom'); ?>
+<?php
+global $post;
+$slug = $post->post_name;
+    if ($slug == 'showroom' ) {
+        $category = get_category_by_slug('showroom');
+        $children = get_term_children($category->term_id, $category->taxonomy);
+    }
+    else
+//        print_r($slug);
+        echo ('keep trying idiot' . $slug);
+?>
 <div class="container">
     <section class="row">
         <h2 class="col-sm-4"><?php echo $category->name ?></h2>
@@ -7,7 +17,8 @@
     </section>
     <div class="row">
         <?php
-            print_r($category);
+//        print_r($category);
+            print_r($children);
 //        $query = new WP_Query(array( 'category_name' => 'showroom'));
 //        while ($query->have_posts()){
 //            $query->the_post();
