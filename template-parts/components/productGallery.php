4f9fa5ec9e6f7283d32ba909c841046b0a2e4ef3
jbadach02 <joshuabadach@gmail.com>
2022-06-21 12:58:00 -0600

    got the category names returning

--- template-parts/components/productGallery.php
+++ template-parts/components/productGallery.php
@@ -4,34 +4,23 @@ global $post;
 $slug = $post->post_name;
     if ($slug == 'showroom' ) {
         $category = get_category_by_slug('showroom');
-        $children = get_term_children($category->term_id, $category->taxonomy);
     }
-    else
-//        print_r($slug);
-        echo ('keep trying idiot' . $slug);
+    elseif ($slug == 'parts-and-accessories'){
+        $category = get_category_by_slug('parts-and-accessories');
+    }
 ?>
 <div class="container">
     <section class="row">
-        <h2 class="col-sm-4"><?php echo $category->name ?></h2>
+        <h2 class="col-sm-6"><?php echo $category->name ?></h2>
         <p class="col-sm-12"><?php echo $category->description ?></p>
     </section>
     <div class="row">
         <?php
-//        print_r($category);
-            print_r($children);
-//        $query = new WP_Query(array( 'category_name' => 'showroom'));
-//        while ($query->have_posts()){
-//            $query->the_post();
-//            echo $query;
-//        }
-//        wp_reset_postdata();
-        ?>
-        <div class="categoryCard col-sm-3">
-            <a href="#">
-                <img src="http://placekitten.com/200/200" alt="placeholder">
-                <p>Category</p>
-            </a>
-        </div>
-    </div>
+        $categories = get_term_children($category->term_id, $category->taxonomy);
+        foreach($categories as $category) {
+                $cat = get_term($category, 'category');
+                echo '<div class="col-sm-3 categoryCard"><p>' . $cat->name . '</p></div>';
+            } ?>
+   </div>
 </div>
 
