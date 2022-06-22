9354af4e602f47f567ded0ea25311386dd8048f1
jbadach02 <joshuabadach@gmail.com>
2022-06-22 15:44:26 -0600

    turns out it was gutenburg screwing with the shortcode command

--- custom-shortcodes.php
+++ custom-shortcodes.php
@@ -1,51 +1,4 @@
 <?php
-//$slug = $post->post_name;
-//
-//if ( is_home() ){
-//    $query = new WP_Query(array( 'category_name' => 'slider+home'));
-//    return $query;
-//}
-//elseif ( $slug == 'showroom' ){
-//    $query = new WP_Query(array( 'category_name' => 'slider+showroom'));
-//    return $query;
-//}
-//elseif ($slug == 'parts-and-accessories') {
-//    $query = new WP_Query(array( 'category_name' => 'slider+parts'));
-//    return $query;
-//}
-//elseif ($slug == 'service') {
-//    $query = new WP_Query(array( 'category_name' => 'slider+service'));
-//    return $query;
-//}
-//;
-
-//function carouselContent( $atts, $content = null ){
-//    while ($query->have_posts()){
-//        $query->the_post();
-//        }
-//        return ['<div class="sliderContent">' .
-//                the_post_thumbnail() .
-//                    '<div class="sliderText">' .
-//                    '<h2>' .
-//                        the_title() .
-//                    '</h2>' .
-//                    the_content() .
-/*                    '<button onclick="location.href=\'<?php echo($post->post_name) ?>\' ">Read More</button>' .*/
-//                '</div>'
-//        ];
-//        wp_reset_postdata();
-//}
-//add_shortcode('carousel', 'carouselContent');
-
-//function shortcode_test_function($atts, $content = null) {
-//    $default = array(
-//        'link' => '#',
-//    );
-//    $a = shortcode_atts($default, $atts);
-//    $content = do_shortcode($content);
-//    return 'Follow us on <a href="'.($a['link']).'" style="color: blue">'.$content.'</a>';
-//}
-add_shortcode('test_shortcode', 'shortcode_test_function');
 
 function brands_shortcode(){
     return include('template-parts/components/brands.php');
@@ -53,7 +6,9 @@ function brands_shortcode(){
 add_shortcode('brands', 'brands_shortcode');
 
 function carousel_shortcode(){
-    return include('template-parts/components/carousel.php');
+    ob_start();
+    $carousel = include_once('template-parts/components/carousel.php');
+    return $carousel;
 }
 add_shortcode('carousel', 'carousel_shortcode');
 
@@ -68,7 +23,8 @@ function insta_shortcode(){
 add_shortcode('instagram', 'insta_shortcode');
 
 function product_gallery_shortcode(){
-    return include('template-parts/components/productGallery.php');
+    $gallery = include('template-parts/components/productGallery.php');
+    return $gallery;
 }
 add_shortcode('product-gallery', 'product_gallery_shortcode');
 
