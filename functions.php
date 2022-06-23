91ded8d10c065b17d8c521f14b6568a073f5ab4c
jbadach02 <joshuabadach@gmail.com>
2022-06-23 09:46:15 -0600

    moved everything into functions.php, starting to tweak the shortcode for product loop

--- functions.php
+++ functions.php
@@ -94,5 +94,43 @@ endif;
 
 add_action( 'after_setup_theme', 'rpsShoreside_setup');
 
-include('custom-shortcodes.php');
+//Shortcodes
+
+function carousel_shortcode(){
+    $carousel = include('template-parts/components/carousel.php');
+    return $carousel;
+}
+add_shortcode('carousel', 'carousel_shortcode');
+
+
+function brands_shortcode(){
+    $brands = include('template-parts/components/brands.php');
+    return $brands;
+}
+add_shortcode('brands', 'brands_shortcode');
+
+function hero_shortcode(){
+    $hero = include('template-parts/components/heroVideo.php');
+    return $hero;
+}
+add_shortcode('hero', 'hero_shortcode');
+
+function insta_shortcode(){
+    $insta = include('template-parts/components/instagram.php');
+    return $insta;
+}
+add_shortcode('instagram', 'insta_shortcode');
+
+function product_gallery_shortcode(){
+    $gallery = include('template-parts/components/productGallery.php');
+    return $gallery;
+}
+add_shortcode('product-gallery', 'product_gallery_shortcode');
+
+function quote_shortcode(){
+    $quote = include('template-parts/components/quote.php');
+    return $quote;
+}
+add_shortcode('quote', 'quote_shortcode');
+
 
