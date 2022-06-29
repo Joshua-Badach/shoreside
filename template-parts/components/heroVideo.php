74ed8e63b3738ec8194b50423cf8b51ca554979b
jbadach02 <joshuabadach@gmail.com>
2022-06-29 11:54:51 -0600

    test to see if gitis ok with this hero video now

--- template-parts/components/heroVideo.php
+++ template-parts/components/heroVideo.php
@@ -2,6 +2,6 @@
 
 <div class="heroVideo">
     <video autoplay loop muted>
-        <source src="<?php echo get_template_directory_uri() . '/assets/src/library/videos/VideoPlaceholder.mp4" type="video/mp4' ?>">
+        <source src="<?php echo get_template_directory_uri() . '/assets/src/library/videos/hero.mp4" type="video/mp4' ?>">
     </video>
 </div>
