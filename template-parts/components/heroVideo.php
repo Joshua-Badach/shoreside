830428bb0ede3ea08b637d713a4099844d4a908e
jbadach02 <joshuabadach@gmail.com>
2022-06-30 16:00:33 -0600

    starting on media query break points, a litte demoralized after the server issue on alpha deploy

--- template-parts/components/heroVideo.php
+++ template-parts/components/heroVideo.php
@@ -1,7 +1,8 @@
 <!--change this to dynamic content later-->
 
 <div class="heroVideo">
-    <video autoplay loop muted>
+    <video preload="auto" autoplay loop muted>
         <source src="<?php echo get_template_directory_uri() . '/assets/src/library/videos/hero.mp4" type="video/mp4' ?>">
+        Your browser does not support the video tag.
     </video>
 </div>
