69ecd80c2b322702cf2fd94cec17f15afba9bbd6
jbadach02 <joshuabadach@gmail.com>
2022-07-06 14:04:05 -0600

    brands component generating ocntent from wordpress rather than hardcoded, still need to work through the categories for shorestation and mercury

--- template-parts/components/brands.php
+++ template-parts/components/brands.php
@@ -1,66 +1,72 @@
 <?php
 
-$brands_cat_id = get_cat_ID('brands');
-$brands_cat_link = get_category_link($brands_cat_id);
-$brands_cat_name = get_cat_name($brands_cat_id);
-$brands_cat_children = get_term_by('slug', $brands_cat_name, 'product_cat');
+//idObj is set to 55 because that is the top level boat category
 
+$idObj = 55;
 
-$term_children = get_terms(
-        'brands', array(
-                'parent' => get_queried_object_id(),
-    )
+$taxonomy       = 'product_cat';
+$orderby        = 'name';
+$show_count     = 0;      // 1 for yes, 0 for no
+$pad_counts     = 0;      // 1 for yes, 0 for no
+$hierarchical   = 1;      // 1 for yes, 0 for no
+$title          = '';
+$empty          = 0;
+
+$args = array(
+    'taxonomy'                  => $taxonomy,
+    'orderby'                   => $orderby,
+    'show_count'                => $show_count,
+    'pad_counts'                => $pad_counts,
+    'hierarchical'              => $hierarchical,
+    'title_li'                  => $title,
+    'hide_empty'                => $empty
 );
 
+//$brands_cat_id = get_cat_ID('brands');
+//$brands_cat_link = get_category_link($brands_cat_id);
+//$brands_cat_name = get_cat_name($brands_cat_id);
+//$brands_cat_children = get_term_by('slug', $brands_cat_name, 'product_cat');
+
+//can clean this up but will do that later, works well enough for now
+function brand_loop($idObj, $args){
+    $all_categories = get_categories( $args );
+    $test = ['Avalon', 'MirroCraft', 'Mercury', 'Shorestation' ];
+    echo '<section class="container"> 
+                <div class="row brandSpan">
+                <h2>Our Brands</h2>';
+                foreach ($all_categories as $cat) {
+                    if ($cat->category_parent == $idObj && $cat->name === $test[0] ) {
+                        brand_cards($cat);
+                        }
+                    if ($cat->category_parent == $idObj && $cat->name === $test[1] ) {
+                        brand_cards($cat);
+                    }
+                    if ($cat->category_parent == $idObj && $cat->name === $test[2] ) {
+                        brand_cards($cat);
+                    }
+                    if ($cat->category_parent == $idObj && $cat->name === $test[3] ) {
+                        brand_cards($cat);
+                    }
+                }
+                echo '</section>';
+}
+
+function brand_cards($cat){
+    $thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true );
+    $image = wp_get_attachment_url( $thumbnail_id );
+    echo '<section class="col-sm-3">
+            <a href="' . get_term_link($cat->slug, 'product_cat') . '">
+                <div class="brands">
+                    <div class="brandImage">
+                        <img src="'. $image . '" width="150px" height="150px">
+                    </div>   
+                    <h3>' . $cat->name . '</h3>
+                    <p>' . $cat->description . '</p> 
+                </div>    
+            </a>
+    </section>';
+}
+
+brand_loop($idObj, $args);
+
 ?>
-
-<section class="row brandSpan">
-    <h2>Our Brands</h2>
-    <?php echo $brands_cat_children ?>
-    <section class="col-sm-3">
-        <a href="/avalon/">
-            <div class="brands">
-                <div class="brandImage">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/avalon-logo-gray.png' ?>" alt="Avalon pontoons">
-                </div>
-                <h3>Avalon</h3>
-                <p>The Avalon brand is known for innovative, exciting, and luxurious pontoons. Avalon is a vertically integrated builder, meaning they make almost everything in-house from the pontoons, to framing, to furniture. Avalon luxury pontoons and tritoons are backed by a 10 year bow-to-stern warranty and a lifetime structural warranty. They also include high quality features that most other brands don’t offer or have.</p>
-            </div>
-        </a>
-    </section>
-    <section class="col-sm-3">
-        <a href="/mirrocraft/">
-            <div class="brands">
-                <div class="brandImage">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Mirrocraft-Logo.png' ?>" alt="Mirrocraft fishing boats">
-                </div>
-                <h3>Mirrocraft</h3>
-                <p>The MirroCraft brand has become known for boat innovation, safety, and quality. We are proud to carry the full lineup of MirroCraft aluminum fishing boats and believe that the quality and price is unmatched by competitors. These boats are designed with performance and style in mind, so are perfect for both families new to boat ownership and experienced fishermen.</p>
-            </div>
-        </a>
-    </section>
-    <section class="col-sm-3">
-        <a href="/mercury/">
-            <div class="brands">
-                <div class="brandImage">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Mercury_Lockup.png' ?>" alt="Mercury outboard motors">
-                </div>
-                <h3>Mercury</h3>
-                <p>Mercury Marine is a leader in the industry and a staple among boaters and boat manufacturers. We are proud to offer the full lineup of Mercury outboards and kickers. Purchase with confidence knowing all new motors are backed by a Mercury 3 year Limited Warranty.</p>
-            </div>
-        </a>
-    </section>
-    <section class="col-sm-3">
-        <a href="/shorestation/">
-            <div class="brands">
-                <div class="brandImage">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/Shorestation.png' ?>" alt="Shorestation docking">
-                </div>
-                <h3>Shorestation</h3>
-                <p>ShoreStation is known for producing high quality and dependable waterfront equipment. Our boat lift systems and docks are made to last and backed by one of the best warranties in the business.</p>
-            </div>
-        </a>
-    </section>
-
-
-
