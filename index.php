ab67bbfb7d7fd5b472c701987d44e056a0612b09
jbadach02 <joshuabadach@gmail.com>
2022-07-06 16:30:45 -0600

    odds and ends

--- index.php
+++ index.php
@@ -2,12 +2,8 @@
 
     if ( have_posts() ) :
         while ( have_posts() ) : the_post();
-            the_content(); ?>
-        <p>index returned</p>
-        <?php
+            the_content();
         endwhile;
-    else:
-        _e( 'Sorry, no pages matched your criteria.', 'textdomain');
     endif;
 
 get_footer(); ?>
