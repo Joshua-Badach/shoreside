2c7c881fbf966593faef100425e35a6064040c05
jbadach02 <joshuabadach@gmail.com>
2022-07-29 13:45:47 -0600

    how did I miss that...? must be day before a long weekend, brand content pulling as intended

--- template-parts/components/brandContent.php
+++ template-parts/components/brandContent.php
@@ -1,10 +1,10 @@
 <?php
-//global $post;
+global $post;
 $slug = $post->post_name;
 $idObj = $id->term_id;
 
 $brandsQuery = new WP_Query(array(
-    'category_name'     => 'brands-'.$slug,
+    'category_name'     =>  $slug,
     'order'             => 'DESC',
     'post_status'       => ' publish',
     'posts_per_page'    => 1
@@ -14,7 +14,7 @@ echo '<section class="container">
             <div class="row">';
                 while ($brandsQuery->have_posts()){
                     $brandsQuery->the_post(); ?>
-                        <h2 class="col-sm-3 offset-sm-9"><?php the_title(); ?> </h2>
+                        <h2 class="col-sm-3 offset-sm-9"><?php the_title() ?> </h2>
             </div>
             <div class="row">
                 <p class="col-sm-12"> <?php the_content(); ?></p>
