9b87810161e349008d22616f26026d1b9e8c2fe9
jbadach02 <joshuabadach@gmail.com>
2022-08-12 08:02:51 -0600

    small fixes here and there.

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -31,7 +31,6 @@ do_action( 'woocommerce_before_main_content' );
 
 ?>
     <header class="woocommerce-products-header">
-        product-archive.php
         <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 <!--            <h1 class="woocommerce-products-header__title page-title">--><?php //woocommerce_page_title(); ?><!--</h1>-->
         <?php endif; ?>
