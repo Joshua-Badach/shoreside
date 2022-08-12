0283e48a2415750c0fb843d4be08f37c5296e38b
jbadach02 <joshuabadach@gmail.com>
2022-08-12 08:37:38 -0600

    fixed the instagram component and styling

--- template-parts/components/instagram.php
+++ template-parts/components/instagram.php
@@ -5,7 +5,11 @@ $slug = $post->post_name;
 <div class="container">
     <div class="row">
         <p class="col-12 followUs">
-            <span>Follow Us</span> <img src=<?php echo get_template_directory_uri() . '/assets/src/library/images/instagram.png' . ' width="50" height="50" alt="Instagram icon"> ' ?> <a href="https://www.instagram.com/recreationalpowersports/?hl=en" target="_blank"><span>@recreationalpowersports</span></a>
+            <a href="https://www.instagram.com/recreationalpowersports/?hl=en" target="_blank">
+                <span>Follow Us</span>
+                <img src=<?php echo get_template_directory_uri() . '/assets/src/library/images/instagram.png' . ' width="50" height="50" alt="Instagram icon" description="">' ?>
+                <span>@recreationalpowersports</span>
+            </a>
         </p>
     </div>
 </div>
