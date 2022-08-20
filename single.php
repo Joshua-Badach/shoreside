d7c34b2dc5547cbaca4c7100b34b8f154b6a07fb
jbadach02 <joshuabadach@gmail.com>
2022-08-19 21:06:31 -0600

    single.php conditional

--- single.php
+++ single.php
@@ -1,10 +1,17 @@
-<?php get_header() ?>
+<?php get_header();
+
+global $wp;
+$url = home_url( $wp->request );
+$checkFor = 'product-category';
+?>
 
 <div class="container">
     <?php
         if ( have_posts() ) {
             while ( have_posts() ) {
-                echo '<h2 class="col-6 mx-auto singleheading">' . get_the_title() . '</h2>';
+                if ( strpos($url, $checkFor) == true) {
+                    echo '<h2 class="col-6 mx-auto singleheading">' . get_the_title() . '</h2>';
+                }
                 the_post();
                 the_content();
             }
