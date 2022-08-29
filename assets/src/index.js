99f1f92ff35f9b6c8bae0493494b9f7d718134e2
jbadach02 <joshuabadach@gmail.com>
2022-08-29 12:23:39 -0600

    I may have a fix for the script loading issue, need to test it on live. Only thing is I'm getting a webpack warning for size. time to find out

--- assets/src/index.js
+++ assets/src/index.js
@@ -3,5 +3,8 @@ import 'normalize-css';
 import 'bootstrap-4-grid';
 import 'mini-svg-data-uri';
 import 'slick-carousel';
+import "slick-carousel/slick/slick.css";
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.min.js'
 import './index.scss';
 import './js/app.js';
