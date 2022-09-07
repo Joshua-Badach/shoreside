8856a9a40f390dee99624d5d001a4336e00e3166
jbadach02 <joshuabadach@gmail.com>
2022-09-07 11:06:08 -0600

    confounded by woocommerce, trying to get the product thumbnails to fire

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -76,6 +76,107 @@ jQuery(document).ready(function() {
     mobileFirst: true,
     dots: false,
   });
+  jQuery('.carousel-product-nav').slick({
+    slidesToShow: 6,
+    slidesToScroll:1,
+    arrows: false,
+    asNavFor: '.carousel-product',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+  });
+
+  // if ( jQuery('.carousel-product').length ) {
+  //   var $slider = jQuery('.carousel-product')
+  //     .on('init', function(slick) {
+  //       jQuery('.carousel-product').fadeIn(1000);
+  //     })
+  //     .slick({
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1,
+  //       arrows: true,
+  //       autoplay: true,
+  //       lazyLoad: 'ondemand',
+  //       autoplaySpeed: 3000,
+  //       asNavFor: '.carousel-product-nav'
+  //     });
+  //
+  //   var $slider2 = jQuery('.carousel-product-nav')
+  //     .on('init', function(slick) {
+  //       jQuery('.carousel-product-nav').fadeIn(1000);
+  //     })
+  //     .slick({
+  //       slidesToShow: 4,
+  //       slidesToScroll: 1,
+  //       lazyLoad: 'ondemand',
+  //       asNavFor: '.carousel-product',
+  //       dots: false,
+  //       centerMode: false,
+  //       focusOnSelect: true
+  //     });
+  //
+  //   //remove active class from all thumbnail slides
+  //   jQuery('.carousel-product-nav .slick-slide').removeClass('slick-active');
+  //
+  //   //set active class to first thumbnail slides
+  //   jQuery('.carousel-product-nav .slick-slide').eq(0).addClass('slick-active');
+  //
+  //   // On before slide change match active thumbnail to current slide
+  //   jQuery('.carousel-product').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+  //     var mySlideNumber = nextSlide;
+  //     jQuery('.carousel-product-nav .slick-slide').removeClass('slick-active');
+  //     jQuery('.carousel-product-nav .slick-slide').eq(mySlideNumber).addClass('slick-active');
+  //   });
+  //
+  //
+  //   // init slider
+  //   require(['js-sliderWithProgressbar'], function(slider) {
+  //
+  //     jQuery('.carousel-product').each(function() {
+  //
+  //       me.slider = new slider(jQuery(this), options, sliderOptions, previewSliderOptions);
+  //
+  //       // stop slider
+  //       //me.slider.stop();
+  //
+  //       // start slider
+  //       //me.slider.start(index);
+  //
+  //       // get reference to slick slider
+  //       //me.slider.getSlick();
+  //
+  //     });
+  //   });
+  //   var options = {
+  //     progressbarSelector    : '.bJS_progressbar'
+  //     , slideSelector        : '.bJS_slider'
+  //     , previewSlideSelector : '.bJS_previewSlider'
+  //     , progressInterval     : ''
+  //     // add your own progressbar animation function to sync it i.e. with a video
+  //     // function will be called if the current preview slider item (".b_previewItem") has the data-customprogressbar="true" property set
+  //     , onCustomProgressbar : function($slide, $progressbar) {}
+  //   }
+  //
+  //   // slick slider options
+  //   // see: https://kenwheeler.github.io/slick/
+  //   var sliderOptions = {
+  //     slidesToShow   : 1,
+  //     slidesToScroll : 1,
+  //     arrows         : false,
+  //     fade           : true,
+  //     autoplay       : true
+  //   }
+  //
+  //   // slick slider options
+  //   // see: https://kenwheeler.github.io/slick/
+  //   var previewSliderOptions = {
+  //     slidesToShow   : 3,
+  //     slidesToScroll : 1,
+  //     dots           : false,
+  //     focusOnSelect  : true,
+  //     centerMode     : true
+  //   }
+  // }
 
   checkWidth(); // excute function to check width on load
   jQuery(window).resize(checkWidth); // execute function to check width on resize
--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -64,14 +64,20 @@ $wrapper_classes   = apply_filters(
 
 		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
 
-        $attachment_ids = $product->get_gallery_image_ids();
 
-//        if ( $attachment_ids && $product->get_image_id() ) {
-//            foreach ( $attachment_ids as $attachment_id ) {
-//                echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
-//            }
-//        }
-//		do_action( 'woocommerce_product_thumbnails' );
-		?>
+        ?>
 	</figure>
+    <?php
+    $attachment_ids = $product->get_gallery_image_ids();
+
+    if ( $attachment_ids && $product->get_image_id() ) {
+        echo '<div class="carousel-product-nav">';
+        foreach ( $attachment_ids as $attachment_id ) {
+            echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+        }
+        echo '</div>';
+    }
+    do_action( 'woocommerce_product_thumbnails' );
+    ?>
 </div>
+<!--<div class="carousel-product-nav"></div>-->
