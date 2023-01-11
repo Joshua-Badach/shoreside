7dd0cc1b9a0bb027cd6e8db6b247e27e2260a7eb
jbadach02 <joshuabadach@gmail.com>
2023-01-11 15:56:17 -0700

    Working on the facebook og metadata, got home and any category populated page, now working on individual products and niche cases like the information pages

--- header.php
+++ header.php
@@ -4,8 +4,9 @@
 global $wp;
 $current_url = home_url( add_query_arg( array(), $wp->request ) );
 $site_name = get_bloginfo( 'name' );
-$site_slug = $post->post_name;
-$site_suffix = ucwords(str_replace('-', ' ', $site_slug));
+$slug = $post->post_name;
+$id = get_term_by('slug', $slug, 'product_cat');
+$site_suffix = ucwords(str_replace('-', ' ', $slug));
 $featured = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 
 ?>
@@ -14,28 +15,41 @@ $featured = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'si
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<!--Remove this a week after major pushes-->
-<!--    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">-->
-<!--    <meta http-equiv="Pragma" content="no-cache">-->
-<!--    <meta http-equiv="Expires" content="0">-->
-
     <meta property="fb:app_id" content="568883651374703" />
     <meta property="og:title" content="<?php echo $site_name . ' - ' . $site_suffix ?>" />
     <meta property="og:url" content="<?php echo $current_url ?>" />
     <meta property="og:type" content="website" />
-    <meta property="og:description" content="<?php echo $current_url ?>" />
-    <meta property="og:image" content="<?php echo $featured[0] ?>" />
-
-
+    <meta property="og:locale" content="en_CA" />
     <?php
-    if ( get_post_type() === 'page' ){
+        if (is_home()|| is_front_page()){
+            $description            =           get_post_custom_values('description', $site_slug);
+            $image_id               =           get_page_by_title('rps-logo-share', OBJECT, 'attachment');
+            $image                  =           $image_id->guid;
+        }  else {
+            $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
+            if ($_REQUEST['term'] == '') {
+                if ($_REQUEST['product_cat'] != '') {
+                    $idObj = $_REQUEST['product_cat'];
+                    $field = 'id';
+                    $taxonomy = 'product_cat';
+                } else {
+                    $idObj = $id->term_id;
+                    $field = 'id';
+                    $taxonomy = 'product_cat';
+                }
+            } elseif($_REQUEST['term'] != '') {
+                $idObj = $_REQUEST['term'];
+                $field = 'term_id';
+                $taxonomy = 'pa_manufacturer';
+            }
 
-    }
-    if ( get_post_type() === 'post' ){
+        }
 
-    }
-    ?>
-    <?php wp_head(); ?>
+    $term = get_term_by($field, $idObj, $taxonomy);
+    $description = $term->description;
+    echo '<meta property="og:description" content="' . $description[0] . '"/>
+            <meta property="og:image" content="'. $image . '" />';
+        wp_head(); ?>
 </head>
 <body itemscope itemtype="https://schema.org/Store">
 <?php get_template_part('template-parts/components/news') ?>
@@ -48,3 +62,4 @@ $featured = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'si
 <header>
     <?php get_template_part( 'template-parts/components/nav' ); ?>
 </header>
+<?php var_dump($term);
