44294718612479c823eea55602d3a05746b5c631
jbadach02 <joshuabadach@gmail.com>
2023-01-16 15:32:20 -0700

    Got the sharing cards working for queries properly

--- header.php
+++ header.php
@@ -3,10 +3,9 @@
 <?php
 global $wp;
 
-$current_url = home_url( add_query_arg( array(), $wp->request ) );
+$current_url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $site_name = get_bloginfo( 'name' );
 $slug = $post->post_name;
-$site_suffix = ucwords(str_replace('-', ' ', $slug));
 $featured = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 
 $field = 'slug';
@@ -14,9 +13,6 @@ $value = $slug;
 $taxonomy = 'product';
 
 $id = get_term_by($field, $value, $taxonomy);
-
-// First check for custom value description
-
 ?>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
@@ -24,8 +20,6 @@ $id = get_term_by($field, $value, $taxonomy);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <meta property="fb:app_id" content="568883651374703" />
-    <meta property="og:title" content="<?php echo $site_name . ' - ' . $site_suffix ?>" />
-    <meta property="og:url" content="<?php echo $current_url ?>" />
     <meta property="og:type" content="website" />
     <meta property="og:locale" content="en_CA" />
     <?php
@@ -34,10 +28,19 @@ $id = get_term_by($field, $value, $taxonomy);
         $image_id = get_page_by_title('rps-logo-share', OBJECT, 'attachment');
         $image = $image_id->guid;
         $description = get_post_custom_values('description', $id);
+        $site_suffix = $post->post_title;
 
     } elseif ( get_term_by('slug', $slug, 'product_cat') != '' ) {
-        $catId = get_term_by('slug', $slug, 'product_cat');
-        $description[0] = $catId->description;
+        if ($_REQUEST['product_cat'] != '' && $_REQUEST['term'] == '' ){
+            $descId = get_term_by('id', $_REQUEST['product_cat'], 'product_cat');
+        } elseif ( $_REQUEST['term'] != '' ) {
+            $descId = get_term_by('term_id', $_REQUEST['term'], 'pa_manufacturer');
+        } else {
+            $descId = get_term_by('slug', $slug, 'product_cat');
+        }
+        $site_suffix = $descId->name;
+
+        $description[0] = $descId->description;
         $image = get_the_post_thumbnail_url();
 
     } elseif ( is_product() ) {
@@ -45,13 +48,14 @@ $id = get_term_by($field, $value, $taxonomy);
         $imageId = $product->get_image_id();
         $imageSrc = wp_get_attachment_image_src( get_post_thumbnail_id($image_id), 'small');
         $image = $imageSrc[0];
+        $site_suffix = $product->get_name();
         $description[] = $product->get_short_description();
     }
-
+    echo '<meta property="og:url" content="' . $current_url . '" />';
+    echo '<meta property="og:title" content="' . $site_name . ' - ' . $site_suffix . '" />';
     echo '<meta property="og:description" content="' . filter_var($description[0], FILTER_SANITIZE_STRING) . '"/>';
     echo '<meta property="og:image" content="'. $image . '" />';
     wp_head();
-//    var_dump($catId);
     ?>
 </head>
 <body itemscope itemtype="https://schema.org/Store">
