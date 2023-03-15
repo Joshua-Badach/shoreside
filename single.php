e14b8f8d8c2f2558da0c26df876a47be1b8fb7a4
Joshua Badach <joshuabadach@gmail.com>
2023-03-15 13:32:14 -0600

    got form returning title and url, need to make a confirmation email, page styling for posts done, fallback for no featured posts

--- single.php
+++ single.php
@@ -1,20 +1,28 @@
 <?php
 global $post;
+$banner = get_the_post_thumbnail($post->ID);
+$fallback = get_page_by_title('rps-logo-share', OBJECT, 'attachment');
+$image_alt = get_post_meta($fallback->ID, '_wp_attachment_image_alt', TRUE);
+$image = $fallback->guid;
+
 
 get_header();
-
-    echo '<div class="container">
-        <div class="row">';
+echo '<div class="bannerImage">';
+    echo ( get_the_post_thumbnail() )? ($banner) : ('<img src="'. $image . '" alt="' . $image_alt . '">');
+echo '</div>';
         if ( have_posts() ) {
             while ( have_posts() ) {
+                echo '<div class="container postContainer">
+                    <div class="row">';
                 if( in_category('Careers')){
                     $jobUrl = get_permalink( $post->ID );
                     $jobName = get_the_title( $post->ID );
 
-                    echo '<div class="col-sm-6">';
+                    echo '<section class="col-sm-6">';
+                        echo'<h2>' . $jobName . '</h2>';
                         the_post();
                         the_content();
-                    echo '</div>';
+                    echo '</section>';
                     echo'<div class="col-sm-6">
                         <script type="text/javascript" src="https://form.jotform.com/jsform/230734498938067?jobName=' . $jobName . '&jobUrl=' . $jobUrl .'"></script>
                     </div>';
