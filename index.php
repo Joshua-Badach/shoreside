69300ea00135bad24f76719d3df17584cea1e8ed
Joshua Badach <joshuabadach@gmail.com>
2023-04-20 14:29:53 -0600

    got top level cat videos loading

--- index.php
+++ index.php
@@ -7,20 +7,17 @@ $slug = $post->post_name;
 
 if ($slug != 'search') {
     echo '<div id="shelf">';
+    (isset($_REQUEST['product_cat']) != '') ? ($term = get_term_by('id', $_REQUEST['product_cat'])) : ($term = get_term_by('slug', $slug, 'product_cat'));
+    $term_id = $term->term_id;
+    $cat_video = implode(get_term_meta($term_id, 'cat_video', false));
 
-    if(isset($_REQUEST['product_cat']) != ''){
-        $term = get_term_by('id', $_REQUEST['product_cat'], 'product_cat');
-        $term_id = $term->term_id;
-        $cat_video = implode(get_term_meta($term_id, 'cat_video', false));
-
-        if ($cat_video != '') {
+    if($cat_video != ''){
             echo '<div id="videoTab"><img src="' . get_template_directory_uri() . '/assets/src/library/images/play-button.webp' . '" alt="Video Icon"></div>
                 <div id="videoSlider" style="display: none">
                     <iframe width="100%" height="100%"
                         src="https://www.youtube.com/embed/' . $cat_video . '">
                     </iframe>
                 </div>';
-        }
     }
     echo '</div>';
     echo '<div class="modal" style="display: none">
